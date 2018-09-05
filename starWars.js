542f80cbf430f89180cf436e74156b83f168ed70
nullhack <kimtkevin@gmail.com>
2018-09-04 19:01:08 -0700

    need to fix font color

--- starWars.js
+++ starWars.js
@@ -2,6 +2,7 @@ var name;
 var data;
 var planetUrl
 var movieList;
+var moviesList
 function readName(){
 	 name = document.getElementById("usrInput").value;
 	axios.get('https://swapi.co/api/people/?search='+name).then(function(response){	
@@ -9,12 +10,15 @@ function readName(){
 	document.getElementById("height").innerHTML =  data.height;
 	document.getElementById("mass").innerHTML =data.mass;
 	document.getElementById("hair_color").innerHTML= data.hair_color
+	document.getElementById("skin_color").innerHTML=data.skin_color
+	document.getElementById("gender").innerHTML=data.gender
 	axios.get(data.homeworld).then(function(res){
 	document.getElementById("homeworld").innerHTML=res.data.name;
 	})
         axios.get(data.species).then(function(re){
 	document.getElementById("species").innerHTML=re.data.name
 	})
+
 	let length = data.films.length
 	let promises = [];
 		for(let i = 0; i < length; i++)
@@ -23,16 +27,19 @@ function readName(){
 		}
 
 		Promise.all(promises).then(function (value){
-			console.log(value);
-			console.log(value.length)
-			
-			for(var j=0; j <5; j++)
-			{      
+		//	console.log(value);
+		//	console.log(value.length)
+			moviesList=value
+			console.log(moviesList)
+		        document.getElementById("movies0").innerHTML =value[0].data.title	
+			for(var j=0; j <value.length; j++)
+			{       console.log("hello world")
 				console.log("movies"+j)
 				document.getElementById("movies"+j).innerHTML = value[j].data.title
 			}
 
 		})
+	//	console.log(moviesList)
 	
 //	for( i=0; i<length; i++ )
 //		{ 
@@ -48,11 +55,7 @@ function readName(){
 //			
 //		}
                    
-	
-
-
-
-})
+	})
 	
 }
 
